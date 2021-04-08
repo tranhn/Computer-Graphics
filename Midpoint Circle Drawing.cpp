d449a92ef1e4ced3e4e35c1ea87e563c0bd0284c
tranhn <80198899+tranhn@users.noreply.github.com>
2021-04-08 14:03:56 +0700

    Update Midpoint Circle Drawing.cpp

--- Midpoint Circle Drawing.cpp
+++ Midpoint Circle Drawing.cpp
@@ -3,27 +3,26 @@
 using namespace std;
 
 int r = 100;
-int X1, Y1;
 
 void init(void)
 {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
 }
 
 void putPixel(int cx, int cy, int x, int y)
 {
 	glColor3f(2.0, 0.5, 1.0);
 	glBegin(GL_POINTS);
-	glVertex2i(cx + x, cy + y); 
+	glVertex2i(cx + x, cy + y);
 	glVertex2i(cx + y, cy + x);
-	glVertex2i(cx - x, cy + y); 
+	glVertex2i(cx - x, cy + y);
 	glVertex2i(cx + y, cy - x);
-	glVertex2i(cx + x, cy - y); 
+	glVertex2i(cx + x, cy - y);
 	glVertex2i(cx - y, cy + x);
-	glVertex2i(cx - x, cy - y); 
+	glVertex2i(cx - x, cy - y);
 	glVertex2i(cx - y, cy - x);
 	glEnd();
 	glFlush();
@@ -58,25 +57,35 @@ void MidPointCircle(int X1, int Y1)
 	}
 }
 
+void mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		y = 480 - y;
+		glColor3f(1.0, 0.0, 1.0);
+		glBegin(GL_POINTS);
+		glVertex2f(x, y);
+		glEnd();
+		glFlush();
+		MidPointCircle(x, y);
+	}
+}
+
 void display(void)
 {
-	MidPointCircle(X1, Y1);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 
 int main(int argc, char** argv)
 {
-	cout << "Enter X value of the circle center: ";
-	cin >> X1;
-	cout << "Enter Y value of the circle center: ";
-	cin >> Y1;
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(640, 480);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Circle");
 	init();
-	glutDisplayFunc(display);
+	glutDisplayFunc(&display);
+	glutMouseFunc(&mouse);
 	glutMainLoop();
 	return 0;
 }
